d418eeab1ba21e3489c15f7358dd464c34b49c86
Noah Santer <personal@mail.mossy-tech.com>
2019-03-08 13:48:28 -0500

    Remove build.ignore each time but keep FILES.prev

--- dist/dist.sh
+++ dist/dist.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+[ -d build.ignore ] && rm -r build.ignore
 mkdir -p build.ignore
 cd build.ignore
 
 [ -f LOG ] && cp LOG LOG.prev
-[ -f FILES ] && cp FILES FILES.prev
 
 cp ../PKGBUILD .
 
@@ -16,7 +16,8 @@ echo "Building version $(sed -n 's/pkgver=\(.*\)/\1/p' PKGBUILD)"
     &>LOG || \
     { echo 'Failed, check LOG' >&2 ; exit 1 ; }
 bsdtar -tvf *.pkg.* | grep -v '\.[A-Z]\+' >FILES
-[ -f FILES.prev ] && diff FILES FILES.prev
-ls *.pkg.*
+[ -f ../FILES.prev.ignore ] && diff FILES ../FILES.prev.ignore
+cp FILES ../FILES.prev.ignore
 cp *.pkg.* ..
+ls *.pkg.*
 
